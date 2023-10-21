1b52c3076f4220efd579131f7e2ed5c026366507
Vishal <vishalpal1501@gmail.com>
2023-10-21 22:48:59 +0530

    create the input field and copy button

--- Password Ganerator/src/App.jsx
+++ Password Ganerator/src/App.jsx
@@ -1,9 +1,24 @@
+// import { useState } from 'react';
 function App() {
-  // const [count, setCount] = useState(0)
+  // const [setLength, setLength] = useState(8);
 
   return (
     <>
-      <h1></h1>
+      <div className="w-full max-w-md max-auto shadow-md rounded-lg px-4 my-8 text-orange-700 bg-gray-700">
+        <h1 className="text-white text-center py-4 ">Password Genarator</h1>
+        <div className="flex shadow rounded-lg  overflow-hidden mb-4">
+          <input
+            type="text"
+            // value={password}
+            className="outline-none w-full py-1 px-3"
+            readOnly
+          />
+          <button className="outline-none bg-blue-950 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+        <div className=""></div>
+      </div>
     </>
   );
 }

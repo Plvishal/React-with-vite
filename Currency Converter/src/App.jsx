d64aa64a2428deddec79989f9c1521d71aef75ef
Vishal <vishalpal1501@gmail.com>
2023-10-22 09:13:18 +0530

    Initial commit

---
+++ Currency Converter/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+import './App.css';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl m-5 p-4 bg-rose-600 rounded-2xl text-white">
+        Currency Converter
+      </h1>
+    </>
+  );
+}
+
+export default App;
